08850ccbaa9e363347b4eeba1ee418702c7012b4
opussf <github@zz9-za.com>
2017-09-13 16:43:23 -0700

    optparser needs to be in __main__ block to avoid pydoc conflict

--- src/pyCal.py
+++ src/pyCal.py
@@ -7,21 +7,21 @@ Representations of the next few days, and a record of what was done can be obtai
 
 This will require the ability to add items, tasks, meetings, etc.
 Importance, severity, date due, etc. can be represented and tracked.
-
 """
-
-import optparse
 
 import sqlite3
 conn = sqlite3.connect('pyCal.db')
 
-optparser = optparse.OptionParser( usage="%prog [options]", version="$prog @VERSION@" )
-optparser.add_option( "-n", "--new", action="store_true", dest="newItem", default=False,
-		help="make a new entry" )
-optparser.add_option( "-2", "--todo", action="store_true", dest="toDo", default=False,
-		help="todo flag" )
-optparser.add_option( "-b", "--birthday", action="store_true", dest="birthday", default=False,
-		help="birthday flag" )
+if __name__=="__main__":
+	import optparse
 
+	optparser = optparse.OptionParser( usage="%prog [options]", version="%prog @VERSION@" )
+	optparser.add_option( "-n", "--new", action="store_true", dest="newItem", default=False,
+			help="make a new entry" )
+	optparser.add_option( "-2", "--todo", action="store_true", dest="toDo", default=False,
+			help="todo flag" )
+	optparser.add_option( "-b", "--birthday", action="store_true", dest="birthday", default=False,
+			help="birthday flag" )
 
-(options, args) = optparser.parse_args()
+	(options, args) = optparser.parse_args()
+
